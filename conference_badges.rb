4d08845fce03ea8689e830e8058a382e1aac744d
fliblip <wickynjy@gmail.com>
2022-10-23 18:52:40 +0300

    Completed assignment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator arr
+    arr.map{|name|badge_maker(name)}
+end
+
+def assign_rooms arr
+    arr.map.with_index{|name, ind|"Hello, #{name}! You'll be assigned to room #{ind + 1}!"}
+end
+
+def printer arr
+    arr.map{|name|puts badge_maker(name)}
+    arr.map.with_index{|name, ind|puts "Hello, #{name}! You'll be assigned to room #{ind + 1}!"}
+end
